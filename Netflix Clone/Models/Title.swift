cbc30e6a4422fa27c9e0ed4f1c3b8345d2683c21
Preet Pambhar <preetpambhar73530@gmail.com>
2024-06-19 19:52:07 -0400

    feat: upcoming screen

--- Netflix Clone/Models/Title.swift
+++ Netflix Clone/Models/Title.swift
@@ -14,8 +14,8 @@ struct TrendingTitleResponse: Codable {
 struct Title: Codable {
     let id: Int
     let media_type: String?
-    let orignal_name: String?
-    let orignal_title: String?
+    let original_name: String?
+    let original_title: String?
     let poster_path: String?
     let overview: String?
     let vote_count: Int
---
+++ Netflix Clone/ViewModels/TitleViewModel .swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel .swift
+//  Netflix Clone
+//
+//  Created by Preet Pambhar on 2024-06-19.
+//
+
+import Foundation
+
+struct TitleViewModel{
+    let titleName: String
+    let posterURL: String
+}
